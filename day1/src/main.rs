e550d907ad520679cb8d72cc68ffbf62f0a073d8
Marius Krämer <marius@yazio.com>
2022-12-01 21:20:01 +0100

    avoid sorting repeatedly

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,14 +8,16 @@ impl Elf {
     fn from_string (s: &str) -> Self {
         Elf {calories: s.lines().map(|l| l.parse::<i32>().unwrap()).collect()}
     }
+
+    fn get_summed_calories (&self) -> i32 {
+        self.calories.iter().sum()
+    }
 }
 
 fn get_calories_of_nth_most_packed_elves (elves: &[Elf], num: usize) -> i32 {
     elves
         .iter()
-        .map(|elf| elf.calories.iter().sum::<i32>())
-        .sorted()
-        .rev()
+        .map(|elf| elf.get_summed_calories())
         .take(num)
         .sum()
 }
@@ -25,6 +27,7 @@ fn main() {
 
     let elves: Vec<Elf> = input_str.split("\n\n")
         .map(Elf::from_string)
+        .sorted_by_key(|b| std::cmp::Reverse(b.get_summed_calories()))
         .collect();
 
     println!(
